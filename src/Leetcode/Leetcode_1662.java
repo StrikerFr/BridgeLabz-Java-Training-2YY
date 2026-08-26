8ce2051056b1da57bcb80ff74c57cbafcf6ccbef
Aryan <Aryann@gmail.com>
2026-08-26 13:40:07 +0530

    check

---
+++ src/Leetcode/Leetcode_1662.java
@@ -0,0 +1,9 @@
+package String_01.Leetcode;
+class Leetcode_1662 {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String s1 = String.join("", word1);
+        String s2 = String.join("", word2);
+
+        return s1.equals(s2);
+    }
+}
